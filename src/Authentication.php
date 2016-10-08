e9496f172105f1f9e28e52887181a05282ee56f6
Davey Shafik <dshafik@akamai.com>
2016-10-07 23:57:10 -0700

    Update copyright year

--- src/Authentication.php
+++ src/Authentication.php
@@ -6,7 +6,7 @@
  * {@see https://developer.akamai.com/introduction/Client_Auth.html}
  *
  * @author Davey Shafik <dshafik@akamai.com>
- * @copyright Copyright 2015 Akamai Technologies, Inc. All rights reserved.
+ * @copyright Copyright 2016 Akamai Technologies, Inc. All rights reserved.
  * @license Apache 2.0
  * @link https://github.com/akamai-open/edgegrid-auth-php
  * @link https://developer.akamai.com
